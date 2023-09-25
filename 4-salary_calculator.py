0edc3246db071b336fdeb4ad3b0bf20669d314f8
maciasrcdaniel <maciasrcdaniel@gmail.com>
2023-09-25 06:02:32 +0000

    Salary Calculator App

---
+++ 4-salary_calculator.py
@@ -0,0 +1,6 @@
+# ask user for hours worked and hourly rate
+hours_worked = float(input('How many hours did you work last month? '))
+hourly_rate = float(input('What is your hourly rate? '))
+
+# show calculated salary
+print('Last month, you earned', hours_worked * hourly_rate, 'dollars')
